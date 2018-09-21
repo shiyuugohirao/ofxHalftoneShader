d6421277c22c2ec7ebd6e020760786f05681a63c
shugohirao <>
2018-09-21 13:39:28 +0900

    [modify] dotInterval, dotSize, aaRate; [modify] calcurate Depth by brightness or luminance;

--- src/ofxHalftoneShader.h
+++ src/ofxHalftoneShader.h
@@ -11,15 +11,19 @@
 class ofxHalftoneShader {
 private:
     ofShader shader;
-    float dotSize = 10.0;
-    float ratio = 5.0;
-    float threshold = 0.3; //0.0~1.0
+    float dotInterval = 10.0;   // interval
+    float dotSize = 8.0;        // diameter(dotInterval = maxSize)
+    float aaRate = 1.05;        // AntiAliasingRate
+    float threshold = 0.3;      // threshold of depth (0.0~1.0)
+    bool useLuminance = false;  // use (true)Luminace / (false)Brightness
+
 public:
     void setup(){
         setupShader();
     }
 
-    void process(ofFbo& inout) { process(inout.getTexture(), inout); }
+    //[*] deprecated (Under construction)
+    //void process(ofFbo& inout) { process(inout.getTexture(), inout); }
 
     void process(ofTexture& in, ofFbo& out)
     {
@@ -27,9 +31,11 @@ public:
         ofClear(0);
         shader.begin();{
             shader.setUniformTexture("tex0", in, 0);
+            shader.setUniform1f("dotInterval", dotInterval);
             shader.setUniform1f("dotSize", dotSize);
-            shader.setUniform1f("ratio", ratio);
+            shader.setUniform1f("aaRate", aaRate);
             shader.setUniform1f("threshold", threshold);
+            shader.setUniform1i("useLuminance", (int)useLuminance);
             ofSetColor(255);
             in.draw(0, 0, out.getWidth(), out.getHeight());
         }
@@ -37,42 +43,52 @@ public:
         out.end();
     }
 
+    void setDotInterval(float dotInterval){ this->dotInterval = dotInterval; }
     void setDotSize(float dotSize){ this->dotSize = dotSize; }
-    void setRatio(float ratio){ this->ratio = ratio; }
+    void setAaRate(float aaRate){ this->aaRate = aaRate; }
     void setThreshold(float threshold){ this->threshold = threshold; }
+    void setUseLuminance(bool b){ useLuminance = b; }
 
-    
+
 protected:
     void setupShader(){
         string fShader =
         OS_STRINGIFY(
                      uniform sampler2DRect tex0;
+                     uniform float dotInterval;
                      uniform float dotSize;
-                     uniform float ratio;
+                     uniform float aaRate;
                      uniform float threshold;
+                     uniform int useLuminance;
                      void main (void){
                          vec2 TC = gl_TexCoord[0].xy;
                          float rads = radians(45.);
+                         float radius = dotSize * 0.5; // diameter/2
                          vec2 coord = TC * mat2(cos(rads), sin(rads), -sin(rads), cos(rads));
-                         vec2 pos = mod(coord, vec2(dotSize)) - vec2(dotSize/2.0);
-                         float d = dot(pos, pos);
+                         vec2 pos = mod(coord, vec2(dotInterval)) - vec2(dotInterval/2.) + vec2(radius);
+                         float radPos = length(pos-vec2(radius));
                          vec4 col = texture2DRect(tex0, TC);
+
                          float bri = max(max(col.r,col.g),col.b);
-                         //float lum = (col.r+col.g+col.b) /3.; // if you need
-                         float v = 1. - bri; // value of strength
-                         float show = step(threshold, v);
+                         float lum = (col.r + col.g + col.b) / 3.;
+                         float bl = mix(bri, lum, float(useLuminance));
+                         float depth = 1. - bl;
+
+                         float show = step(threshold, depth);
+
                          vec4 bCol = vec4(1.,1.,1.,0.);
-                         gl_FragColor = mix(col, bCol, smoothstep(v*v*dotSize, v*v*dotSize*ratio, d)) * show;
+                         float rate = smoothstep(pow(depth,2.)*dotSize, pow(depth,2.)*dotSize*aaRate, radPos);
+                         gl_FragColor = mix(col, bCol, rate) * show;
                      }
                      );
 
         if(shader.setupShaderFromSource(GL_FRAGMENT_SHADER, fShader)) shader.linkProgram();
 
         if(!shader.isLoaded()){
-            cout<<"[halftoneShader] shader.isLoaded() : failed to load"<<endl;
+            cout<<"[ofxHalftoneShader] shader.isLoaded() : failed to load"<<endl;
             std::exit(0);
         }
-        else cout<<"[halftoneShader] shader.isLoaded() : success!!"<<endl;
+        else cout<<"[ofxHalftoneShader] shader.isLoaded() : success!!"<<endl;
     }
 
 };
